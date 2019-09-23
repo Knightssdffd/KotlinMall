f82d1124353a4f760df377bbecbb572a13cf2cd8
=幻忧 <837639041@qq.com>
2019-09-23 11:22:10 +0800

    主页切换完成

--- App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.kotlin.mall.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.ashokvarma.bottomnavigation.BottomNavigationBar
+import com.kotlin.base.ui.activity.BaseActivity
 import com.kotlin.mall.R
 import com.kotlin.mall.ui.fragment.HomeFragment
+import com.kotlin.mall.ui.fragment.MeFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val mStack = Stack<Fragment>()
+    private val mHomeFragment by lazy { HomeFragment() }
+    private val mCategoryFragment by lazy { HomeFragment() }
+    private val mCartFragment by lazy { HomeFragment() }
+    private val mMsgFragment by lazy { HomeFragment() }
+    private val mMeFragment by lazy { MeFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +25,26 @@ class MainActivity : AppCompatActivity() {
         mBottomNavBar.checkCartBadge(10)
         mBottomNavBar.checkMsgBadge(true)
 
-        initView()
+//        initView()
+        initFragment()
+        initBottomNav()
+        changeFragment(0)
+    }
+
+    private fun initFragment() {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.add(R.id.mConatier, mHomeFragment)
+        manager.add(R.id.mConatier, mCategoryFragment)
+        manager.add(R.id.mConatier, mCartFragment)
+        manager.add(R.id.mConatier, mMsgFragment)
+        manager.add(R.id.mConatier, mMeFragment)
+        manager.commit()
+
+        mStack.add(mHomeFragment)
+        mStack.add(mCategoryFragment)
+        mStack.add(mCartFragment)
+        mStack.add(mMsgFragment)
+        mStack.add(mMeFragment)
     }
 
     private fun initView() {
@@ -22,4 +52,31 @@ class MainActivity : AppCompatActivity() {
         manager.replace(R.id.mConatier, HomeFragment())
         manager.commit()
     }
+
+
+    private fun initBottomNav() {
+        mBottomNavBar.setTabSelectedListener(object : BottomNavigationBar.OnTabSelectedListener {
+            override fun onTabReselected(position: Int) {
+
+            }
+
+            override fun onTabUnselected(position: Int) {
+
+            }
+
+            override fun onTabSelected(position: Int) {
+                changeFragment(position)
+            }
+        })
+    }
+
+    private fun changeFragment(position: Int) {
+        val manager = supportFragmentManager.beginTransaction()
+        for (fragment in mStack) {
+            manager.hide(fragment)
+        }
+        manager.show(mStack[position])
+        manager.commit()
+    }
+
 }
